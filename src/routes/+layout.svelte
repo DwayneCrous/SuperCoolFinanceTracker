3ef400f6bc8f3f792a566bf2ed6709dd826af399
Dwayne <91212710+DwayneCrous@users.noreply.github.com>
2025-07-02 21:33:14 +0200

    favicon and title add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,10 @@
     crossorigin="anonymous"
     referrerpolicy="no-referrer"
   />
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <title>SCFTA</title>
 </svelte:head>
 
 <main>
